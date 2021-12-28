0f266833ddc329956363488a496fde01382f28f0
Ziddi-Mian <ahmadyounas893@gmail.com>
2021-12-28 19:28:59 +0500

    some change

--- script.js
+++ script.js
@@ -1,463 +1,402 @@
 // Write a JavaScript function to get lowercase Ante meridiem and Post meridiem.
 
-function lower_am_pm(dt) 
-{ 
-   return dt.getHours() < 12 ? 'AM' : 'PM';
+function lower_am_pm(dt) {
+  return dt.getHours() < 12 ? "AM" : "PM";
 }
 
-dt = new Date(); 
-console.log(lower_am_pm(dt)); 
-
-dt = new Date(1989, 10, 1); 
+dt = new Date();
 console.log(lower_am_pm(dt));
 
+dt = new Date(1989, 10, 1);
+console.log(lower_am_pm(dt));
 
-// 31. Write a JavaScript function to get uppercase Ante meridiem and Post meridiem.   function upper_am_pm(dt) 
-{ 
-    return dt.getHours() < 12 ? 'AM' : 'PM';
-  }
-  
-  dt = new Date(); 
-  console.log(upper_am_pm(dt)); 
-  
-  dt = new Date(1989, 10, 1); 
-  console.log(upper_am_pm(dt));
+// 31. Write a JavaScript function to get uppercase Ante meridiem and Post meridiem.   function upper_am_pm(dt)
+{
+  return dt.getHours() < 12 ? "AM" : "PM";
+}
 
+dt = new Date();
+console.log(upper_am_pm(dt));
 
+dt = new Date(1989, 10, 1);
+console.log(upper_am_pm(dt));
 
-  //   Write a JavaScript function to swatch Internet time (000 through 999).   
+//   Write a JavaScript function to swatch Internet time (000 through 999).
 // Test Data :
 // dt = new Date(1989, 10, 1);
 // console.log(internet_time(dt));
 // 812
 
-function internet_time(dt) 
-{ 
-  return Math.floor((((dt.getUTCHours() + 1) % 24) + dt.getUTCMinutes() / 60 + dt.getUTCSeconds() / 3600) * 1000 / 24);
+function internet_time(dt) {
+  return Math.floor(
+    ((((dt.getUTCHours() + 1) % 24) +
+      dt.getUTCMinutes() / 60 +
+      dt.getUTCSeconds() / 3600) *
+      1000) /
+      24
+  );
 }
 
-dt = new Date(); 
-console.log(internet_time(dt)); 
-
-dt = new Date(1989, 10, 1); 
+dt = new Date();
 console.log(internet_time(dt));
 
-// Write a JavaScript function to get 12-hour format of an hour with leading zeros.   
+dt = new Date(1989, 10, 1);
+console.log(internet_time(dt));
+
+// Write a JavaScript function to get 12-hour format of an hour with leading zeros.
 // Test Data :
 // dt = new Date(1989, 10, 1);
 // console.log(hours_with_zeroes(dt));
 // "12"
 
-function hours_with_zeroes(dt) 
-{ 
-  return ((dt.getHours() % 12 || 12) < 10 ? '0' : '') + (dt.getHours() % 12 || 12);
+function hours_with_zeroes(dt) {
+  return (
+    ((dt.getHours() % 12 || 12) < 10 ? "0" : "") + (dt.getHours() % 12 || 12)
+  );
 }
 
-dt = new Date(); 
-console.log(hours_with_zeroes(dt)); 
-
-dt = new Date(1989, 10, 1); 
+dt = new Date();
 console.log(hours_with_zeroes(dt));
 
-// Write a JavaScript function to get 24-hour format of an hour without leading zeros.   
+dt = new Date(1989, 10, 1);
+console.log(hours_with_zeroes(dt));
+
+// Write a JavaScript function to get 24-hour format of an hour without leading zeros.
 // Test Data :
 // dt = new Date(1989, 10, 1);
 // console.log(hours_without_zeroes(dt));
 // 0]
 
-
-function hours_without_zeroes(dt) 
-{ 
+function hours_without_zeroes(dt) {
   return dt.getHours();
 }
 
-dt = new Date(); 
-console.log(hours_without_zeroes(dt)); 
-
-dt = new Date(1989, 10, 1); 
+dt = new Date();
 console.log(hours_without_zeroes(dt));
 
+dt = new Date(1989, 10, 1);
+console.log(hours_without_zeroes(dt));
 
-// Write a JavaScript function to get minutes with leading zeros (00 to 59).   
+// Write a JavaScript function to get minutes with leading zeros (00 to 59).
 // Test Data :
 // dt = new Date(1989, 10, 1);
 // console.log(minutes_with_leading_zeros(dt));
 // "00"
 
-
-function minutes_with_leading_zeros(dt) 
-{ 
-  return (dt.getMinutes() < 10 ? '0' : '') + dt.getMinutes();
+function minutes_with_leading_zeros(dt) {
+  return (dt.getMinutes() < 10 ? "0" : "") + dt.getMinutes();
 }
 
-dt = new Date(); 
-console.log(minutes_with_leading_zeros(dt)); 
-
-dt = new Date(1989, 10, 1); 
+dt = new Date();
 console.log(minutes_with_leading_zeros(dt));
 
+dt = new Date(1989, 10, 1);
+console.log(minutes_with_leading_zeros(dt));
 
-
-// Write a JavaScript function to get seconds with leading zeros (00 through 59).   
+// Write a JavaScript function to get seconds with leading zeros (00 through 59).
 // Test Data :
 // dt = new Date(1989, 10, 1);
 // console.log(seconds_with_leading_zeros(dt));
 // "00"
 
-function seconds_with_leading_zeros(dt) 
-{ 
-  return (dt.getMinutes() < 10 ? '0' : '') + dt.getMinutes();
+function seconds_with_leading_zeros(dt) {
+  return (dt.getMinutes() < 10 ? "0" : "") + dt.getMinutes();
 }
 
-dt = new Date(); 
-console.log(seconds_with_leading_zeros(dt)); 
-
-dt = new Date(1989, 10, 1); 
+dt = new Date();
 console.log(seconds_with_leading_zeros(dt));
 
-// Write a JavaScript function to get Timezone.   
+dt = new Date(1989, 10, 1);
+console.log(seconds_with_leading_zeros(dt));
+
+// Write a JavaScript function to get Timezone.
 // Test Data :
 // dt = new Date();
 // console.log(seconds_with_leading_zeros(dt));
 // "India Standard Time"
 
-function seconds_with_leading_zeros(dt) 
-{ 
+function seconds_with_leading_zeros(dt) {
   return /\((.*)\)/.exec(new Date().toString())[1];
 }
 
-dt = new Date(); 
-console.log(seconds_with_leading_zeros(dt)); 
-
-dt = new Date(1989, 10, 1); 
+dt = new Date();
 console.log(seconds_with_leading_zeros(dt));
 
+dt = new Date(1989, 10, 1);
+console.log(seconds_with_leading_zeros(dt));
 
-// Write a JavaScript function to find whether or not the date is in daylights savings time.   
+// Write a JavaScript function to find whether or not the date is in daylights savings time.
 // Test Data :
 // dt = new Date();
 // console.log(daylights_savings(dt));
 // 1
 
-function daylights_savings(dt) 
-{ 
+function daylights_savings(dt) {
   var dst = null;
-  for (var i = 0; i < 12; ++i) 
-    {
-      var d = new Date(dt.getFullYear(), i, 1);
-      var offset = dt.getTimezoneOffset();
+  for (var i = 0; i < 12; ++i) {
+    var d = new Date(dt.getFullYear(), i, 1);
+    var offset = dt.getTimezoneOffset();
 
-      if (dst === null) 
-        dst = offset;
-      else if (offset < dst) 
-         {
-           dst = offset; 
-           break;
-         } 
-      else if (offset > dst) 
-        break;
-         }
-return (dt.getTimezoneOffset() == dst) | 0;
+    if (dst === null) dst = offset;
+    else if (offset < dst) {
+      dst = offset;
+      break;
+    } else if (offset > dst) break;
+  }
+  return (dt.getTimezoneOffset() == dst) | 0;
 }
 
-dt = new Date(); 
-console.log(daylights_savings(dt)); 
-
-dt = new Date(1989, 10, 1); 
+dt = new Date();
 console.log(daylights_savings(dt));
 
+dt = new Date(1989, 10, 1);
+console.log(daylights_savings(dt));
 
-// Write a JavaScript function to get difference to Greenwich time (GMT) in hours.   
+// Write a JavaScript function to get difference to Greenwich time (GMT) in hours.
 // Test Data :
 // dt = new Date();
 // console.log(diff_to_GMT(dt));
 // "+05.500"
 
-function diff_to_GMT(dt) 
-{ 
-   return (-dt.getTimezoneOffset() < 0 ? '-' : '+') + (Math.abs(dt.getTimezoneOffset() / 60) < 10 ? '0' : '') + (Math.abs(dt.getTimezoneOffset() / 60)) + '00';
+function diff_to_GMT(dt) {
+  return (
+    (-dt.getTimezoneOffset() < 0 ? "-" : "+") +
+    (Math.abs(dt.getTimezoneOffset() / 60) < 10 ? "0" : "") +
+    Math.abs(dt.getTimezoneOffset() / 60) +
+    "00"
+  );
 }
 
-dt = new Date(); 
-console.log(diff_to_GMT(dt)); 
-
-dt = new Date(1989, 10, 1); 
+dt = new Date();
 console.log(diff_to_GMT(dt));
 
-// Write a JavaScript function to get timezone offset in seconds.   
+dt = new Date(1989, 10, 1);
+console.log(diff_to_GMT(dt));
+
+// Write a JavaScript function to get timezone offset in seconds.
 // Note : The offset for timezones west of UTC is always negative, and for those east of UTC is always positive.
 // Test Data :
 // dt = new Date();
 // console.log(timezone_offset_in_seconds(dt));
 // 19800
 
-function timezone_offset_in_seconds(dt) 
-{ 
-   return -dt.getTimezoneOffset() * 60;
+function timezone_offset_in_seconds(dt) {
+  return -dt.getTimezoneOffset() * 60;
 }
 
-dt = new Date(); 
-console.log(timezone_offset_in_seconds(dt)); 
-
-dt = new Date(1989, 10, 1); 
+dt = new Date();
 console.log(timezone_offset_in_seconds(dt));
 
+dt = new Date(1989, 10, 1);
+console.log(timezone_offset_in_seconds(dt));
 
-// Write a JavaScript function to add specified years to a date.   
+// Write a JavaScript function to add specified years to a date.
 // Test Data :
 // dt = new Date(2014,10,2);
 // console.log(add_years(dt, 10).toString());
 // Output :
 // "Sat Nov 02 2024 00:00:00 GMT+0530 (India Standard Time)"
 
-
-function add_years(dt,n) 
- {
- return new Date(dt.setFullYear(dt.getFullYear() + n));      
- }
+function add_years(dt, n) {
+  return new Date(dt.setFullYear(dt.getFullYear() + n));
+}
 
 dt = new Date();
-console.log(add_years(dt, 10).toString());  
-
-dt = new Date(2014,10,2);
 console.log(add_years(dt, 10).toString());
 
+dt = new Date(2014, 10, 2);
+console.log(add_years(dt, 10).toString());
 
-// Write a JavaScript function to add specified weeks to a date.   
+// Write a JavaScript function to add specified weeks to a date.
 // Test Data :
 // dt = new Date(2014,10,2);
 // console.log(add_weeks(dt, 10).toString());
 // Output :
 // "Sun Jan 11 2015 00:00:00 GMT+0530 (India Standard Time)"
 
-function add_weeks(dt, n) 
- {
- return new Date(dt.setDate(dt.getDate() + (n * 7)));      
- }
+function add_weeks(dt, n) {
+  return new Date(dt.setDate(dt.getDate() + n * 7));
+}
 
 dt = new Date();
-console.log(add_weeks(dt, 10).toString());  
-
-dt = new Date(2014,10,2);
 console.log(add_weeks(dt, 10).toString());
 
+dt = new Date(2014, 10, 2);
+console.log(add_weeks(dt, 10).toString());
 
-
-// Write a JavaScript function to add specified weeks to a date.   
+// Write a JavaScript function to add specified weeks to a date.
 // Test Data :
 // dt = new Date(2014,10,2);
 // console.log(add_weeks(dt, 10).toString());
 // Output :
 // "Sun Jan 11 2015 00:00:00 GMT+0530 (India Standard Time)"
 
-function add_weeks(dt, n) 
- {
- return new Date(dt.setDate(dt.getDate() + (n * 7)));      
- }
+function add_weeks(dt, n) {
+  return new Date(dt.setDate(dt.getDate() + n * 7));
+}
 
 dt = new Date();
-console.log(add_weeks(dt, 10).toString());  
-
-dt = new Date(2014,10,2);
 console.log(add_weeks(dt, 10).toString());
 
+dt = new Date(2014, 10, 2);
+console.log(add_weeks(dt, 10).toString());
 
-// Write a JavaScript function to add specified months to a date.   
+// Write a JavaScript function to add specified months to a date.
 // Test Data :
 // dt = new Date(2014,10,2);
 // console.log(add_months(dt, 10).toString());
 // Output :
 // "Wed Sep 02 2015 00:00:00 GMT+0530 (India Standard Time)"
 
-function add_months(dt, n) 
- {
-
-   return new Date(dt.setMonth(dt.getMonth() + n));      
- }
+function add_months(dt, n) {
+  return new Date(dt.setMonth(dt.getMonth() + n));
+}
 
 dt = new Date();
-console.log(add_months(dt, 10).toString());  
-
-dt = new Date(2014,10,2);
 console.log(add_months(dt, 10).toString());
 
+dt = new Date(2014, 10, 2);
+console.log(add_months(dt, 10).toString());
 
-// Write a JavaScript function to get time differences in minutes between two dates.   
+// Write a JavaScript function to get time differences in minutes between two dates.
 // Test Data :
 // dt1 = new Date("October 13, 2014 11:11:00");
 // dt2 = new Date("October 13, 2014 11:13:00");
 // console.log(diff_minutes(dt1, dt2));
 // 2
 
-function diff_minutes(dt2, dt1) 
- {
-
-  var diff =(dt2.getTime() - dt1.getTime()) / 1000;
+function diff_minutes(dt2, dt1) {
+  var diff = (dt2.getTime() - dt1.getTime()) / 1000;
   diff /= 60;
   return Math.abs(Math.round(diff));
-  
- }
+}
 
-dt1 = new Date(2014,10,2);
-dt2 = new Date(2014,10,3);
+dt1 = new Date(2014, 10, 2);
+dt2 = new Date(2014, 10, 3);
 console.log(diff_minutes(dt1, dt2));
 
-
-// Write a JavaScript function to get time differences in hours between two dates.   
+// Write a JavaScript function to get time differences in hours between two dates.
 // Test Data :
 // dt1 = new Date("October 13, 2014 08:11:00");
 // dt2 = new Date("October 13, 2014 11:13:00");
 // console.log(diff_hours(dt1, dt2));
 // 3
 
-function diff_hours(dt2, dt1) 
- {
-
-  var diff =(dt2.getTime() - dt1.getTime()) / 1000;
-  diff /= (60 * 60);
+function diff_hours(dt2, dt1) {
+  var diff = (dt2.getTime() - dt1.getTime()) / 1000;
+  diff /= 60 * 60;
   return Math.abs(Math.round(diff));
-  
- }
+}
 
-dt1 = new Date(2014,10,2);
-dt2 = new Date(2014,10,3);
+dt1 = new Date(2014, 10, 2);
+dt2 = new Date(2014, 10, 3);
 console.log(diff_hours(dt1, dt2));
-
 
 dt1 = new Date("October 13, 2014 08:11:00");
 dt2 = new Date("October 13, 2014 11:13:00");
 console.log(diff_hours(dt1, dt2));
 
-
-// Write a JavaScript function to get time differences in days between two dates.   
+// Write a JavaScript function to get time differences in days between two dates.
 // Test Data :
 // dt1 = new Date("October 13, 2014 08:11:00");
 // dt2 = new Date("October 19, 2014 11:13:00");
 // console.log(diff_days(dt1, dt2));
 // 6
 
-function diff_days(dt2, dt1) 
- {
-
-  var diff =(dt2.getTime() - dt1.getTime()) / 1000;
-  diff /= (60 * 60 * 24);
+function diff_days(dt2, dt1) {
+  var diff = (dt2.getTime() - dt1.getTime()) / 1000;
+  diff /= 60 * 60 * 24;
   return Math.abs(Math.round(diff));
-  
- }
+}
 
-dt1 = new Date(2014,10,2);
-dt2 = new Date(2014,10,6);
+dt1 = new Date(2014, 10, 2);
+dt2 = new Date(2014, 10, 6);
 console.log(diff_days(dt1, dt2));
 
 dt1 = new Date("October 13, 2014 08:11:00");
 dt2 = new Date("October 19, 2014 11:13:00");
 console.log(diff_days(dt1, dt2));
 
-
-
-// Write a JavaScript function to get time differences in days between two dates.   
+// Write a JavaScript function to get time differences in days between two dates.
 // Test Data :
 // dt1 = new Date("October 13, 2014 08:11:00");
 // dt2 = new Date("October 19, 2014 11:13:00");
 // console.log(diff_days(dt1, dt2));
 // 6
 
-function diff_days(dt2, dt1) 
- {
-
-  var diff =(dt2.getTime() - dt1.getTime()) / 1000;
-  diff /= (60 * 60 * 24);
+function diff_days(dt2, dt1) {
+  var diff = (dt2.getTime() - dt1.getTime()) / 1000;
+  diff /= 60 * 60 * 24;
   return Math.abs(Math.round(diff));
-  
- }
+}
 
-dt1 = new Date(2014,10,2);
-dt2 = new Date(2014,10,6);
+dt1 = new Date(2014, 10, 2);
+dt2 = new Date(2014, 10, 6);
 console.log(diff_days(dt1, dt2));
 
 dt1 = new Date("October 13, 2014 08:11:00");
 dt2 = new Date("October 19, 2014 11:13:00");
 console.log(diff_days(dt1, dt2));
 
-
-// Write a JavaScript function to get time differences in years between two dates.   
+// Write a JavaScript function to get time differences in years between two dates.
 // Test Data :
 // dt1 = new Date("June 13, 2014 08:11:00");
 // dt2 = new Date("October 19, 2017 11:13:00");
 // console.log(diff_years(dt1, dt2));
 // 3
 
-function diff_years(dt2, dt1) 
- {
+function diff_years(dt2, dt1) {
+  var diff = (dt2.getTime() - dt1.getTime()) / 1000;
+  diff /= 60 * 60 * 24;
+  return Math.abs(Math.round(diff / 365.25));
+}
 
-  var diff =(dt2.getTime() - dt1.getTime()) / 1000;
-   diff /= (60 * 60 * 24);
-  return Math.abs(Math.round(diff/365.25));
-   
- }
-
-dt1 = new Date(2014,10,2);
-dt2 = new Date(2014,10,11);
+dt1 = new Date(2014, 10, 2);
+dt2 = new Date(2014, 10, 11);
 console.log(diff_years(dt1, dt2));
 
 dt1 = new Date("June 13, 2014 08:11:00");
 dt2 = new Date("October 19, 2017 11:13:00");
 console.log(diff_years(dt1, dt2));
 
+// Write a JavaScript function to get the week start date.
 
-// Write a JavaScript function to get the week start date.  
+function startOfWeek(date) {
+  var diff = date.getDate() - date.getDay() + (date.getDay() === 0 ? -6 : 1);
 
-function startOfWeek(date)
-  {
-    var diff = date.getDate() - date.getDay() + (date.getDay() === 0 ? -6 : 1);
-  
-    return new Date(date.setDate(diff));
- 
-  }
+  return new Date(date.setDate(diff));
+}
 
-dt = new Date(); 
+dt = new Date();
 
 console.log(startOfWeek(dt).toString());
 
-// Write a JavaScript function to get the week end date.  
+// Write a JavaScript function to get the week end date.
 
-function endOfWeek(date)
-  {
-     
-    var lastday = date.getDate() - (date.getDay() - 1) + 6;
-    return new Date(date.setDate(lastday));
- 
-  }
+function endOfWeek(date) {
+  var lastday = date.getDate() - (date.getDay() - 1) + 6;
+  return new Date(date.setDate(lastday));
+}
 
-dt = new Date(); 
+dt = new Date();
 
 console.log(endOfWeek(dt).toString());
 
-
 // Write a JavaScript function to get the month start date.
 
-function startOfMonth(date)
-  {
-     
-   return new Date(date.getFullYear(), date.getMonth(), 1);
- 
-  }
+function startOfMonth(date) {
+  return new Date(date.getFullYear(), date.getMonth(), 1);
+}
 
-dt = new Date(); 
+dt = new Date();
 
 console.log(startOfMonth(dt).toString());
 
+// Write a JavaScript function to get the month end date.
 
-
-// Write a JavaScript function to get the month end date.  
-
-function endOfMonth(date)
-  {
-     
+function endOfMonth(date) {
   return new Date(date.getFullYear(), date.getMonth() + 1, 0);
- 
-  }
+}
 
-dt = new Date(); 
-
-console.log(endOfMonth(dt).toString()); 
+dt = new Date();
+console.log(endOfMonth(dt).toString());
