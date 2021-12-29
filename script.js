5687b87f88606c2fcc7dd422d47586ecc970f2f3
Ziddi-Mian <ahmadyounas893@gmail.com>
2021-12-30 03:15:53 +0500

    Date Assignment
    a JavaScript function to get the month end date.

--- script.js
+++ script.js
@@ -392,4 +392,11 @@ dt = new Date();
 
 console.log(startOfMonth(dt).toString());
 
+// Write a JavaScript function to get the month end date.
 
+function endOfMonth(date) {
+  return new Date(date.getFullYear(), date.getMonth() + 1, 0);
+}
+
+dt = new Date();
+console.log(endOfMonth(dt).toString());
